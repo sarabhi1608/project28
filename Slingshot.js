f03f0e56d4422c52f5071cbbb098cda564927fb1
sarabhi1608 <67191097+sarabhi1608@users.noreply.github.com>
2020-08-11 12:34:50 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,39 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+  
+    display(){
+    	push();
+    	if(this.sling.bodyA){
+    		var pointA = this.sling.bodyA.position;
+    	    var pointB = this.pointB;
+    		strokeWeight(6);
+    		stroke("white");
+    		line(pointA.x, pointA.y, pointB.x, pointB.y);
+    	}
+        else{
+            textSize(18);
+            fill(0,0,0);
+            text("Press space to Get the ball back", 200, 50);
+        }
+    	pop();
+    }
+    
+}
